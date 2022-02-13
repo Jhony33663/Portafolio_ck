53604a2ae2e99e9fe1ab759c0b2e42cad5407704
Jonathan Mata <jhony33663@gmail.com>
2022-02-13 12:50:20 -0500

    actualizacion 13/02/2022

---
+++ Coop/cooperativapetroecuador.com/portal/wp-content/plugins/whatsapp-for-wordpress/assets/js/whatsapp-popup271b.js
@@ -0,0 +1,15 @@
+(function($) {
+  $(document).ready(function() {
+    $("#wa").njtWhatsApp({
+      accounts: njt_wa.accounts,
+      timezone: njt_wa_global.timezone,
+      gdprStatus: njt_wa.gdprStatus,
+      defaultAvatar: njt_wa_global.defaultAvatarSVG,
+      options: njt_wa.options,
+      urlSettings: njt_wa_global.urlSettings
+    });
+    $("#wa").click(function(e){
+      e.preventDefault()
+    })
+  });
+})($j_njt);
